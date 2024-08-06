e538faf6fdd204b033c439848e4c4734542baf59
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-08-06 13:23:55 +0000

    update navbatr

--- SafePass/src/components/Navbar.jsx
+++ SafePass/src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='bg-slate-800 text-white '>
+        <nav className='bg-purple-100 text-white '>
             <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
 
                 <div className="logo font-bold text-white text-2xl">
-                    <span className='text-green-500'> &lt;</span>
+                    <span className='text-purple-500'> &lt;</span>
                    
-                    <span>Pass</span><span className='text-green-500'>OP/&gt;</span>
+                    <span className='text-black'>Safe</span><span className='text-purple-500'>Pass/&gt;</span>
                   
                     
                     </div>
@@ -19,9 +19,9 @@ const Navbar = () => {
                         <a className='hover:font-bold' href='#'>Contact</a>
                     </li>
                 </ul> */}
-                <button className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'> 
+                <button className='text-white  bg-gradient-to-t from-[#8678f9] from-0% to-[#c7d2fe]  my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'> 
                     <img className='invert  w-10 p-1' src="/icons/github.svg" alt="github logo" />
-                    <span className='font-bold px-2'>GitHub</span>
+                    <a className='font-bold px-2'href="https://github.com/mk016">GitHub</a>
                     
                 </button>
             </div>
