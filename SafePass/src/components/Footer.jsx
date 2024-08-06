e8a04336145dee5e7613d3e0581a2c0243a8d314
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-08-06 13:31:55 +0000

    add footer

---
+++ SafePass/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className=' text-white flex flex-col justify-center items-center  w-full'>
+            <div className="logo font-bold text-white text-2xl">
+                <span className='text-purple-500'> &lt;</span>
+
+                <span>Safe</span><span className='text-purple-500'>Pass/&gt;</span>
+
+
+            </div>
+            <div className='flex justify-center items-center'> Created with by Mahendrakumawat </div>
+        </div>
+    )
+}
+
+export default Footer
